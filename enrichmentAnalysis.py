e91fd97d63565590976c4b6b3bb49a28a5fa1173
Nitesh sabankar <niteshsabankar@gmail.com>
2018-05-18 18:24:02 -0700

    Update enrichmentAnalysis.py

--- enrichmentAnalysis.py
+++ enrichmentAnalysis.py
@@ -1,10 +1,17 @@
-import subprocess
+import os, fnmatch, subprocess
 
-modules = ['brownA2', 'cyanA2', 'darkolivegreenA2', 'greenyellowA2', 'grey60A2', 'lightyellowA2', 'magentaA2', 'pinkA2', 'purpleA2', 'yellowA2']
+modules = []
+listOfFiles = os.listdir('.')
+pattern = "*.ids"
+for entry in listOfFiles:
+	if fnmatch.fnmatch(entry, pattern):
+		modules.append(entry)
+modules.remove("all.ids")
+print(modules)
 
 for module in modules:
-        command = "/w2/xyzhang/src/trinity/Analysis/DifferentialExpression/run_GOseq.pl --genes_single_factor " + module + " --GO_assignments ehux_go.txt --lengths ehux.seq_lens --background background.ids"
-        print command
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        process.wait()
-        print "Return Code for", module, "module is", process.returncode
+	command = "/w2/xyzhang/src/trinity/Analysis/DifferentialExpression/run_GOseq.pl --genes_single_factor " + module + " --GO_assignments geph_go.txt --lengths geph.seq_lens --background all.ids"
+	print command
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	process.wait()
+	print "Return Code for", module, "module is", process.returncode, "\n"
